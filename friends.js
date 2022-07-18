d770a404c1cef073036af03a2982028f9d9c7e4b
Adelle Pitsas <apitsas4@gmail.com>
2022-07-18 18:50:36 -0400

    add an array of places to visit

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var arrays = ["Devany", "Dina", "Blake", "Joe", "Cam"]
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var placesToVisit = ["Ireland", "New Zealand", "Montana", "Greece", "Patagonia"]
+placesToVisit.shift();
+console.log(placesToVisit);
