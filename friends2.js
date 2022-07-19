f0fb46e86f867df82918d44ad28aeb946314c863
Adelle Pitsas <apitsas4@gmail.com>
2022-07-19 16:56:08 -0400

    complete friends2.js

--- friends2.js
+++ friends2.js
@@ -1,9 +1,9 @@
 var bestFriend = "Joe"
+var alsoGoodFriend = "Devany"
 
-bestFriend = "Devany"
-
-if (bestFriend === "Joe") {
+if ((bestFriend === "Joe")&&(alsoGoodFriend === "Devany")) {
   console.log(`Hi ${bestFriend}, you are my best friend`)
+  console.log(`${alsoGoodFriend}, you are not only my best friend but also my sister`)
 }
 else {
   console.log(`Hi ${bestFriend}, you are also my best friend`)
