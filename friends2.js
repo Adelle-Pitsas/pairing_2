d5c792bb89d689753f5aaf3bee5efaa52bbc2b70
Adelle Pitsas <apitsas4@gmail.com>
2022-07-19 16:50:13 -0400

    add friends2 file

---
+++ friends2.js
@@ -0,0 +1,10 @@
+var bestFriend = "Joe"
+
+bestFriend = "Devany"
+
+if (bestFriend === "Joe") {
+  console.log(`Hi ${bestFriend}, you are my best friend`)
+}
+else {
+  console.log(`Hi ${bestFriend}, you are also my best friend`)
+}
